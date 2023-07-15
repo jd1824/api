06dbc0a8f0d3ed9794545e70691466d27c8dd2bc
jd1824 <111320378+jd1824@users.noreply.github.com>
2023-07-15 19:13:53 -0400

    Update index.py
    cambio

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes("Hola mundo!", "utf-8"))
 
-server = http.server.HTTPServer(('localhost', 5000), MyHandler)
+server = http.server.HTTPServer(('0.0.0.0', 5000), MyHandler)
 
 
 server.serve_forever()
