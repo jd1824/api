0f7f1227473c84b19077a068bca96fed129e22f7
Jesus <jdserrano106@gmail.com>
2023-07-15 19:26:39 -0400

    cambio

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("Hola mundo!", "utf-8"))
+        self.wfile.write(bytes("Jesus Serrano", "utf-8"))
 
-server = http.server.HTTPServer(('localhost', 5000), MyHandler)
+server = http.server.HTTPServer(('0.0.0.0', 5000), MyHandler)
 
 
 server.serve_forever()
